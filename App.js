c22a73fe2293ede0f9123f021310a62d7b73cd8d
mohitnegi724 <mohitnegi724@gmail.com>
2019-08-06 13:37:03 +0530

    Heroku Setup

--- App.js
+++ App.js
@@ -17,8 +17,7 @@ mongoose.connect(process.env.MONGO_URI || Keys.mongoURI, { useNewUrlParser: true
     console.log("DB Connected");
 });
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'));
-
+    app.use(express.static(path.join(__dirname, "client", "build")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
